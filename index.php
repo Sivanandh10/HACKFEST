48eb331b089dfc3120dcaec6115af8cad7d6dff3
jayaprakash-e-k <jp008882@gmail.com>
2024-09-03 11:17:44 +0530

    chore: Update navigation styling and dependencies

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
     <link rel="icon" href="./assets/HACKFEST LOGO.png">
     <title>HACKFEST</title>
     <link rel="StyleSheet" href="./assets/css/style.css?<?php echo time(); ?>">
-    <link rel="stylesheet" href="./assets/css/nav-style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -30,7 +33,7 @@
             function updateText() {
                 textElement.innerText = originalText;
             }
-            setInterval(updateText, 4000); 
+            setInterval(updateText, 4000);
         </script>
 
 
@@ -301,13 +304,16 @@
             </ul>
         </div>
 
-        </div>
+    </div>
 
 
     </div>
     </div>
     <div id="scrollToTop" class="scroll-icon">&#8593;</div>
-    <script src="./css js/scroll.js"></script>
+    <script src="./assets/js/scroll.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
